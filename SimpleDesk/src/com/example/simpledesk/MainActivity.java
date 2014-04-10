4190fb339c30a9d6bf07de74971a2921de80f04d
jesus- <chusma.ellagarto@gmail.com>
2014-04-10 18:59:16 +0800

    init commit

--- SimpleDesk/src/com/example/simpledesk/MainActivity.java
+++ SimpleDesk/src/com/example/simpledesk/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -17,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		TextView tv = (TextView)findViewById(R.id.textViewName);
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
